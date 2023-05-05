e8143316301671f3450f98ee0f24ce2b8341f1d9
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-05 13:32:35 +0300

    Footer and Contact us section added

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from './Hero'
 import About from './About'
+import Services from './Services'
+import Contactus from './Contactus'
+import Footer from './Footer'
 
 const Home = () => {
   return (
     <>
     <Hero/>
     <About/>
+    <Services/>
+    <Contactus/>
+    <Footer/>
     </>
   )
 }
