2ac5c43fba91b67a274df4766661b22bd86b7239
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-05 22:35:50 +0300

    Slider section updated

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Hero from './Hero'
 import About from './About'
 import Services from './Services'
 import Contactus from './Contactus'
@@ -9,7 +8,7 @@ const Home = () => {
   return (
     <>
     <Slider/>
-    <Hero/>
+   
     <About/>
     <Services/>
     <Contactus/>
