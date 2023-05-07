98f780547ce7759125beaaf8cd410f2bc0d5e23c
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-07 15:35:07 +0300

    Nav bar height updated

--- src/Components/Navigation/Nav.tsx
+++ src/Components/Navigation/Nav.tsx
@@ -8,37 +8,18 @@ const Nav = () => {
     <div>
         <div className="header_top">
         <div className="container">
-          <div className="row">
-            <div className="col-sm-5">
-              <div className="contact_main">
-                <div className="contact_left">
-                  <div className="call_text"><a href="#">
-                    <img alt="call" src={call}/>
-                    <span className="call_text_left">+01 1234567890</span></a></div>
-                </div>
-                <div className="contact_right">
-                  <div className="call_text">Make an appointment</div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-2">
+          <div className="d-flex justify-content-between items-center align-items-center ">
+            
+            <div>
               <div className="logo">
                 <a href="index.html">
                   <img alt="logo" className='logo-rcmo3d' src={logo_rhmo3d}/></a></div>
             </div>
-            <div className="col-sm-5">
-              <div className="contact_main">
-                <div className="contact_left">
+                
                   <div className="call_text"><a href="#">
                     <img alt="mail" src={mail}/>
                     <span className="call_text_left">business@fioresglobal.com</span></a></div>
-                </div>
-                <div className="contact_right">
-                  <div className="call_text"><a href="#">
-                    <img alt="map" src={map}/><span className="call_text_left">+01 9876543210</span></a></div>
-                </div>
-              </div>
-            </div>
+          
           </div>
         </div>
     </div>
