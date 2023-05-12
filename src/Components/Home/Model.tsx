7ef3ab04c820cd4ab3e40f661a454d38e5de67f2
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-12 16:44:05 +0300

    Model style updated

--- src/Components/Home/Model.tsx
+++ src/Components/Home/Model.tsx
@@ -3,16 +3,16 @@ import model2 from "../../../public/model/kemerli burun.gif"
 
 const Model = () => {
   return (
-    <div className="d-flex flex-column">
+    <div className="d-flex flex-column align-items-center mx-5 ">
       <h1 className="model_taital">Models</h1>
-      <div className="d-flex">
-        <div className="d-flex flex-column align-items-center ">
+      <div className="d-flex gap-3">
+        <div className="d-flex flex-column align-items-center shadow-lg">
         <img alt='model' src={model1}/>
-        <h2 className="model-text">Original Nose</h2>
+        <h2 className="model-text mb-2">Original Nose</h2>
         </div>
-        <div className="d-flex flex-column align-items-center ">
+        <div className="d-flex flex-column align-items-center shadow-lg">
         <img alt='model' src={model2}/>
-        <h2 className="model-text">Arched Nose</h2>
+        <h2 className="model-text mb-2">Arched Nose</h2>
         </div>
       </div>
     </div>
