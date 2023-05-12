2cdbce2e6d73079bd5d08283fa485457c7d73a22
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-12 13:31:26 +0300

    Nav bar with slider image updated

--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -1,12 +1,13 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import comp from "../../../public/images/comp.jpg"
+import slider1 from "../../../public/images/Desktop2.svg"
 const Hero = ()=> {  
         return (
             <div>
-            <Carousel  dynamicHeight={false}  emulateTouch={true} showThumbs={false} axis="vertical" >
-                <div>
-                    <img alt='slider' className='slider-image' src={comp} />
+            <Carousel   dynamicHeight={false}  emulateTouch={true} showThumbs={false} axis="vertical" >
+                <div className='slider-mar'>
+                    <img alt='slider' className='slider-image' src={slider1} />
                     <p className="legend">Rhmo3d</p>
                 </div>
                 <div>
